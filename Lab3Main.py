2692bf688b643716d2b5779172450446024098af
Seth Fenske <fenskesd@gmail.com>
2018-09-20 11:19:23 -0500

    Add files via upload

--- Lab3Main.py
+++ Lab3Main.py
@@ -1,3 +1,33 @@
+<<<<<<< HEAD
+
+
+def print_to_file(list_of_lines, file):
+    """
+    Take the list of lines, and print it to the specified file. Each entry in the list is a line of text,
+    and the file exists.
+
+    list_of_lines: A list with one or more entries. Each entry represents an ASCII string
+    file: The destination file to which we want to print the file
+    """
+
+    return
+
+def get_file_name():
+    """
+    Prompts the user for the name of a file they would like to write the results to.
+    :return: The full path of the file they are trying to write to
+    """
+    return ""
+
+def print_to_file(list_of_lines, file_path):
+    """
+    Prints the message cointainted in list_of_lines to the file at the specified path.
+    
+    :param list_of_lines: A list containing each line of text to write to the file as a respecstive entry.
+    :param file_path:  The full path that points to the file to which we will write
+    :return: None
+    """
+=======
 """
 - CS2911 - 021
 - Fall 2018
@@ -10,6 +40,8 @@
 """
 
 from CS2911L3.Message import Message
+
+
 
 
 def read_message(message):
@@ -40,3 +72,4 @@ def read_next_line(message):
     """
 
     return b''
+>>>>>>> 49c888a6b6d4e51907797dc5f2d7eedcad7ab6de
